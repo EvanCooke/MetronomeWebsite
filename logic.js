8fbbb366c4b95981a153ea6123b6da578f29916b
Evan Cooke <evcooke@uiowa.edu>
2022-02-21 16:43:26 -0600

    Cleaned up files and removed js from index.html

--- logic.js
+++ logic.js
@@ -1,8 +1,16 @@
+// https://stackoverflow.com/questions/10671174/changing-button-text-onclick
+function change()
+{
+    var elem = document.getElementById("startStopButton");
+    if (elem.value=="Start") elem.value = "Stop";
+    else elem.value = "Start";
+}
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("bpmNum");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+output.innerHTML = this.value;
 }
