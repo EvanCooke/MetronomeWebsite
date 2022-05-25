78a4c818b6df8496eafa311249913fa6a29a769b
Evan Cooke <evncooke@gmail.com>
2022-05-25 15:53:00 -0500

    added link to bpm api

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+
+
+# To check if a website allows web scraping or not you can use status_code as follows:
+
+# import requests
+
+# from bs4 import BeautifulSoup 
+
+# r=requests.get(" ENTER URL OF YOUR CHOICE")
+
+# r.status_code
+
+# The output to this should be 200. Anything other than 200 means that the website your trying to scrape either does not allow web scraping or allows partially.
+
+# https://getsongbpm.com/api
+
 from flask import Flask, render_template
 import requests
 import json
